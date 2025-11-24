a9ccbf3669244e621d6c96110fb799f8c7814f13
joevinM <joevin.manceau@hotmail.fr>
2025-11-24 15:50:27 +0100

    Add all files for the task 2

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+This module defines a type-annotated function `concat` that takes two
+strings as arguments and returns their concatenation. This task continues
+the introduction to basic type annotations in Python.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    return str1 + str2
