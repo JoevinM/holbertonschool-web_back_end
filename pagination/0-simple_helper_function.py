5306e7c6a658361e85fb41de6080c545c465b88c
joevinM <joevin.manceau@hotmail.fr>
2025-12-02 15:35:57 +0100

    Add a DOCSTRING to the function 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+"""
+Calculate the start and end indexes for pagination based on the given page
+number and page size. Page numbers start at 1, and the returned tuple
+contains the range of item indexes to be returned for the requested page.
+"""
+
+
 def index_range(page: int, page_size: int) -> tuple:
     """
     Returns a tuple of (start_index, end_index)
