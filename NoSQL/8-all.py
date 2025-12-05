e84c58fc12e745ee8fe1b9d396707e012560ddcd
joevinM <joevin.manceau@hotmail.fr>
2025-12-05 11:50:33 +0100

    Fix nothing for the checker

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -7,10 +7,8 @@ Module that provides a function to list all documents in a MongoDB collection.
 def list_all(mongo_collection):
     """
     List all documents in a MongoDB collection.
-
     Args:
     mongo_collection: pymongo collection object
-
     Returns:
     List of documents (empty list if no document exists)
     """
