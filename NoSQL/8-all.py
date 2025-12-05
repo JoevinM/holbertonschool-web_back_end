3054bd2f0574c69d115d8679bed1726c5899acec
joevinM <joevin.manceau@hotmail.fr>
2025-12-05 11:22:53 +0100

    Add script for the task 8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,20 @@
+#!/bin/bin/env python3
+"""
+Module that provides a function to list all documents in a MongoDB collection.
+"""
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+
+    """
+    List all documents in a MongoDB collection.
+
+    Args:
+        mongo_collection: pymongo collection object
+
+    Returns:
+        List of documents (empty list if no document exists)
+    """
+
+    return list(mongo_collection.find())
