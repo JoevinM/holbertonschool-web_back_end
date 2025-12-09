072cefd9a939e15c12de28c6c4f7ea0c6470fb32
joevinM <joevin.manceau@hotmail.fr>
2025-12-09 15:15:43 +0100

    Add files for the task 2

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,11 @@
+// This module uses an ES6 arrow function to preserve the lexical `this` context,
+// eliminating the need for a temporary variable and ensuring correct access
+// to the object's properties.
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
