e45c4b8fb9dcd01fc31e98d7865bf6b13fb86541
joevinM <joevin.manceau@hotmail.fr>
2025-12-11 16:33:22 +0100

    Add all files for the task 5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+// This define a class name Building
+export default class Building{
+	constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    if (
+      this.constructor !== Building &&
+      this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage
+    ) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+	
+	get sqft() {
+		return this._sqft
+	}
+
+	evacuationWarningMessage() {
+		throw new Error('Class extending Building must override evacuationWarningMessage')
+	}
+}
