c4796b290c1d40b3a40bf55727d317f968185837
joevinM <joevin.manceau@hotmail.fr>
2025-12-11 22:33:39 +0100

    Add all files for the task 8

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,21 @@
+// Define a classe name HolbertonClass
+
+export default class HolbertonClass{
+	constructor(size, location) {
+		if (typeof size !== 'number') {
+			throw new TypeError('size must be a number');
+		}
+		this._size = size;
+		if (typeof location !== 'string') {
+			throw new TypeError('location must be a string');
+		}
+		this._location = location;
+	}
+
+	valueOf() {
+		return this._size;
+	}
+	toString() {
+		return this._location;
+	}
+}
