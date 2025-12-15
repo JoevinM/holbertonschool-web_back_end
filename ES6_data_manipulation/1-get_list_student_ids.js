12be9f40658b4667cb51404c86a81b3bc62136bf
joevinM <joevin.manceau@hotmail.fr>
2025-12-15 17:06:58 +0100

    Add all files for the task 1

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+// Returns an array of student ids
+export default function getListStudentIds(array_students) {
+	if (!Array.isArray(array_students)) {
+		return [];
+	}
+
+	return 	array_students.map((banana)=>[banana.id]);
+}
