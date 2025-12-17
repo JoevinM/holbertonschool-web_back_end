63ad0842ef957608170d0866ed0bfe150aaae81f
joevinM <joevin.manceau@hotmail.fr>
2025-12-17 17:39:58 +0100

    Fix the case of startwith is another type

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
 // return a string of all the set who match with a specific string
 export default function cleanSet(set, startString) {
-	if (startString) {
-		const new_array = [];
-		for (const value of set) {
-			if (value.startsWith(startString)) {
-				new_array.push(value.slice(startString.length));
-			}
-		}
-		return new_array.join("-");
-	}
+	if (typeof startString !== 'string' || startString.length === 0) {
 		return '';
+	}
+	const new_array = [];
+	for (const value of set) {
+		if (value.startsWith(startString)) {
+			new_array.push(value.slice(startString.length));
+		}
+	}
+	return new_array.join("-");
 	}
